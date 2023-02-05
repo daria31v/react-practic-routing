e13c9eba8c592b7502535374485ec61c668388e3
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-02-05 11:50:53 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,7 @@ const Home = () => {
       <h1>Welcome</h1>
       <img src="https://via.placeholder.com/960x240" alt="" />
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
-        laboriosam placeat incidunt rem illum animi nemo quibusdam quia
-        voluptatum voluptate.
+        Information about products.
       </p>
     </main>
   );
