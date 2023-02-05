09843e9482001894ba859c1cde5f6652f8e3869f
daria31v <106913534+daria31v@users.noreply.github.com>
2023-02-05 10:51:40 +0000

    Deploying to gh-pages from @ daria31v/react-practic-routing@e13c9eba8c592b7502535374485ec61c668388e3 🚀

---
+++ static/js/415.2f045382.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsxs)("main",{children:[(0,c.jsx)("h1",{children:"Welcome"}),(0,c.jsx)("img",{src:"https://via.placeholder.com/960x240",alt:""}),(0,c.jsx)("p",{children:"Information about products."})]})}}}]);
+//# sourceMappingURL=415.2f045382.chunk.js.map
